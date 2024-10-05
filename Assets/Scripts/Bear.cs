36fb897a61d84487fb7a18ff078c538f3439aeb5
Dan <me@danielholcomb.com>
2024-10-04 20:15:47 -0400

    bad camera

--- Assets/Scripts/Bear.cs
+++ Assets/Scripts/Bear.cs
@@ -5,8 +5,7 @@ using UnityEngine.InputSystem;
 
 public class Bear : MonoBehaviour
 {
-    [SerializeField]
-    private float moveSpeed;
+    public float moveSpeed;
 
     private Vector2 input;
 
@@ -19,8 +18,12 @@ public class Bear : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector3 targetMovement = new Vector3(input.x, 0f, input.y);
-        rb.velocity = targetMovement * moveSpeed * Time.deltaTime;
+        Vector3 inputVector = new Vector3(input.x, 0f, input.y);
+        Quaternion rotation = Quaternion.FromToRotation(Vector3.forward, transform.forward);
+
+        // Apply the rotation to the vector
+        Vector3 rotatedVector = rotation * inputVector;
+        rb.velocity = rotatedVector * moveSpeed * Time.fixedDeltaTime;
     }
 
     public void Move(InputAction.CallbackContext context)
