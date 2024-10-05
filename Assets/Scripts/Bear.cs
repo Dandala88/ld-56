aec24afd517333106b3da900c1944c5e75a17fb2
Dan <me@danielholcomb.com>
2024-10-04 20:35:47 -0400

    k

--- Assets/Scripts/Bear.cs
+++ Assets/Scripts/Bear.cs
@@ -23,7 +23,6 @@ public class Bear : MonoBehaviour
         Vector3 inputVector = new Vector3(input.x, 0f, input.y);
         Quaternion rotation = Quaternion.FromToRotation(Vector3.forward, transform.forward);
 
-        // Apply the rotation to the vector
         Vector3 rotatedVector = rotation * inputVector;
         rb.velocity = rotatedVector * moveSpeed * Time.fixedDeltaTime;
     }
