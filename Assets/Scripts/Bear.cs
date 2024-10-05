2f48de32bef4299e2ccd8079f585f106bd461fc9
Dan <me@danielholcomb.com>
2024-10-04 23:27:24 -0400

    nope but ok

--- Assets/Scripts/Bear.cs
+++ Assets/Scripts/Bear.cs
@@ -6,11 +6,16 @@ using UnityEngine.InputSystem;
 public class Bear : MonoBehaviour
 {
     public float moveSpeed;
+    public float deceleration;
+    public float pitchSpeed;
+    public float yawSpeed;
     public Camera cam;
     public Laser laserPrefab;
     public Transform laserOrigin;
+    private float pitchYawMoveThreshold = 0.25f;
 
     private Vector2 input;
+    private Vector2 pitchYaw;
 
     private Rigidbody rb;
 
@@ -24,16 +29,33 @@ public class Bear : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector3 inputVector = new Vector3(input.x, 0f, input.y);
-        Quaternion rotation = Quaternion.FromToRotation(Vector3.forward, transform.forward);
+        var finalRotation = Vector2.zero;
+        if (Mathf.Abs(pitchYaw.y) > pitchYawMoveThreshold)
+        {
+            var sign = Mathf.Sign(pitchYaw.y);
+            finalRotation.x = -sign * pitchSpeed * Time.fixedDeltaTime;
+        }
 
-        Vector3 rotatedVector = rotation * inputVector;
-        rb.velocity = rotatedVector * moveSpeed * Time.fixedDeltaTime;
-    }
+        if (Mathf.Abs(pitchYaw.x) > pitchYawMoveThreshold)
+        {
+            var sign = Mathf.Sign(pitchYaw.x);
+            finalRotation.y = sign * yawSpeed * Time.fixedDeltaTime;
+        }
 
-    private void LateUpdate()
-    {
-        transform.rotation = Quaternion.Lerp(transform.rotation, cam.transform.rotation, 6f * Time.deltaTime);
+        transform.Rotate(finalRotation);
+
+        if (Mathf.Abs(input.y) > 0.1f)
+        {
+            var sign = Mathf.Sign(input.y);
+            rb.AddForce(transform.forward * sign * moveSpeed * Time.fixedDeltaTime);
+        }
+
+        if (Mathf.Abs(input.x) > 0.1f)
+        {
+            var sign = Mathf.Sign(input.x);
+            rb.AddForce(transform.right * sign * moveSpeed * Time.fixedDeltaTime);
+        }
+        rb.velocity = Vector3.MoveTowards(rb.velocity, Vector3.zero, Time.fixedDeltaTime * deceleration);
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -49,5 +71,12 @@ public class Bear : MonoBehaviour
             clone.transform.position = transform.position;
             clone.transform.forward = transform.forward;
         }
+    }
+
+    public void PitchYaw(InputAction.CallbackContext context)
+    {
+        pitchYaw = context.ReadValue<Vector2>();
+        pitchYaw.x = Mathf.Clamp(pitchYaw.x, -1f, 1f);
+        pitchYaw.y = Mathf.Clamp(pitchYaw.y, -1f, 1f);
     }
 }
